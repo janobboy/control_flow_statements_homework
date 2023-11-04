f47d009aee90c315f3ebf3bba692084b10767613
janobboy <qulamatovjanob@gmail.com>
2023-11-04 16:50:13 +0500

    code.

--- if01.py
+++ if01.py
@@ -1,9 +1,6 @@
 def main(a):
-    """
-    If the number is positive, increase it to 1,otherwise leave unchanged.
-    Args:
-        a: integer
-    Returns:
-        a: a increased by 1 if positive, else unchanged.
-    """
-    return
+    if a>0:
+     a += 1
+    return a
+print(main(1))
+print(main(-5))
